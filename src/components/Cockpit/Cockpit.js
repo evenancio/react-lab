02ef0258f7f369604728c644c6d4f647ff1c5eeb
Evandro Venancio <evandro.venancio@emanagecrm.com>
2018-09-26 17:54:43 -0300

    Adding Auxiliary

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Aux from '../../hoc/Auxiliary';
 
 
 const cockpit = ( props ) => {
     
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
 
     if(props.showPersons) {
-        btnClass = classes.Red;
+        btnClass = [classes.Button, classes.Red].join(' ');
     }
     
     if(props.persons.length <= 2){
@@ -20,13 +21,13 @@ const cockpit = ( props ) => {
     }
 
     return(
-        <div className={classes.Cockpit}>
+        <Aux>
             <h1>Hello</h1>
             <p className={assignedClasses.join(' ')}>It's working</p>
             <button 
                 className={btnClass}
                 onClick={props.clicked}>Click here</button>
-        </div>
+        </Aux>
     );
 };
 
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -11,4 +11,6 @@ const person = (props) => {
     );
 };
 
+
+
 export default person;
